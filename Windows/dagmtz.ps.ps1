7a8e03b51fcb1f7f5ca6ad7609f0cb7de1032b6d
dagmtz <dagmtzs@gmail.com>
2024-10-07 22:26:03 -0600

    Separate files for Windows/Linux and add some more

---
+++ Windows/dagmtz.ps.ps1
@@ -0,0 +1,5 @@
+oh-my-posh --init --shell pwsh --config D:\Config\Windows\PowerShell\dagmtz.omp.json | Invoke-Expression
+
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineOption -EditMode Windows
